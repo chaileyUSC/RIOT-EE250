32679979e45906c414d14e3bb64ffffe879d8885
CS104 Student User <hailey@usc.edu>
2018-01-30 14:15:04 -0800

    hi

--- examples/udp_rx_tx/udp.c
+++ examples/udp_rx_tx/udp.c
@@ -237,6 +237,8 @@ void print_rss(msg_t *msg)
         */
 
         /* Tell GNRC you are done with this packet so it can release the memory */
+
+        printf ("rss: %d\n", hdr->rssi - CC2538_RSSI_OFFSET);
         gnrc_pktbuf_release(pkt);
     }
 }
@@ -250,6 +252,8 @@ void print_prr(uint32_t pkt_rcv, uint32_t num_pkts)
      * of packets you actually received. Calculate the Packet Reception Ratio 
      * and print it out 
      */
+
+    printf ("prr: %f", ((float) pkt_rcv)/((float)num_pkts));
     
 
 }
